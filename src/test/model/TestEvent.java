27bdf9d70d589a61d636d764108b18e8c485c0c8
daksh <dmathu01@students.cs.ubc.ca>
2023-10-13 12:47:05 -0700

    - Added the details of my project which answer the 3 basic questions given under Task 2 of Phase 0 of the project. - Added the required number and types of user stories - Starting phase 1: First commit

--- src/test/model/TestEvent.java
+++ src/test/model/TestEvent.java
@@ -10,61 +10,55 @@ public class TestEvent {
 
     @BeforeEach
     public void setUp() {
-        event = new Event("Graduation", "05/20/2023", "John's college graduation");
+        event = new Event("Graduation", "08/10/2026", "Lily's college graduation");
     }
 
     @Test
     public void testGetters() {
         assertEquals("Graduation", event.getEventName());
-        assertEquals("05/20/2023", event.getEventDate());
-        assertEquals("John's college graduation", event.getDescription());
+        assertEquals("08/10/2026", event.getEventDate());
+        assertEquals("Lily's college graduation", event.getDescription());
     }
-
 
     @Test
     public void testSetters() {
         event.setEventName("Birthday Party");
-        event.setEventDate("03/10/2023");
-        event.setDescription("Alice's birthday celebration");
-
+        event.setEventDate("02/20/2023");
+        event.setDescription("Grace's birthday celebration");
         assertEquals("Birthday Party", event.getEventName());
-        assertEquals("03/10/2023", event.getEventDate());
-        assertEquals("Alice's birthday celebration", event.getDescription());
+        assertEquals("02/20/2023", event.getEventDate());
+        assertEquals("Grace's birthday celebration", event.getDescription());
     }
-
-
 
     @Test
     public void testToString() {
-        Event event = new Event("Birthday Party", "03/10/2023", "Alice's birthday celebration");
-        String expected = "Event{Event Name='Birthday Party', Event Date='03/10/2023', Event Description='Alice's birthday celebration'}";
+        Event event = new Event("Bachelor Party", "13/10/2023", "Alice's bachelor party celebration");
+        String expected = "Event{Event Name='Bachelor Party', Event Date='13/10/2023', Event Description='Alice's bachelor party celebration'}";
         assertEquals(expected, event.toString());
     }
 
-
-
     @Test
     public void testEventEquals() {
-        Event sameEvent = new Event("Graduation", "05/20/2023", "John's college graduation");
-        Event differentEvent = new Event("Birthday Party", "03/10/2023", "Alice's birthday celebration");
-
-        assertFalse(event.equals(sameEvent)); // Event equals another event with the same properties
-        assertFalse(event.equals(differentEvent)); // Event does not equal an event with different properties
+        Event sameEvent = new Event("Graduation", "05/11/2023", "John's college graduation");
+        Event differentEvent = new Event("Birthday Party", "24/08/2023", "Mary's birthday celebration");
+        assertFalse(event.equals(sameEvent));
+        assertFalse(event.equals(differentEvent));
     }
-
+/*
     @Test
     public void testNullEquality() {
-        assertFalse(event.equals(null)); // Event does not equal null
+        assertFalse(event.equals(null));
     }
 
     @Test
     public void testDifferentObjectEquality() {
-        assertFalse(event.equals(new Object())); // Event does not equal a different object
+        assertFalse(event.equals(new Object()));
     }
 
     @Test
     public void testSameObjectEquality() {
         Event sameObject = event;
-        assertTrue(event.equals(sameObject)); // Event equals the same object
+        assertTrue(event.equals(sameObject));
     }
+ */
 }
