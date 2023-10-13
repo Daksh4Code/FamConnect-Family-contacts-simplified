79e525162600395973203e6147938bc4d8c1d5b3
daksh <dmathu01@students.cs.ubc.ca>
2023-10-13 11:50:22 -0700

    - Added the details of my project which answer the 3 basic questions given under Task 2 of Phase 0 of the project. - Added the required number and types of user stories - Starting phase 1: First commit

--- src/test/model/TestFamilyContactManager.java
+++ src/test/model/TestFamilyContactManager.java
@@ -131,6 +131,159 @@ public class TestFamilyContactManager {
         FamilyContactManager sameObject = contactManager;
         assertTrue(contactManager.equals(sameObject));
     }
+    @Test
+    public void testGetPersonByNameNotFound() {
+        Person person1 = new Person("Alice", "Sister", "2000-01-01", "alice@example.com", "123-456-7890");
+        contactManager.addPerson(person1);
+
+        Person resultNotFound = contactManager.getPersonByName("Bob");
+        assertNull(resultNotFound);
+    }
 
 
+
+    @Test
+    public void testAddNullPerson() {
+        // Adding a null person should do nothing
+        contactManager.addPerson(null);
+        List<Person> contacts = contactManager.getAllContacts();
+
+        assertTrue(contacts.isEmpty());
+    }
+
+    @Test
+    public void testAddMultiplePersons() {
+        // Adding multiple valid persons
+        Person person1 = new Person("Alice", "Sister", "02/20/1985", "alice@email.com", "987-654-3210");
+        Person person2 = new Person("Bob", "Brother", "01/10/1990", "bob@email.com", "123-456-7890");
+
+        contactManager.addPerson(person1);
+        contactManager.addPerson(person2);
+        List<Person> contacts = contactManager.getAllContacts();
+
+        assertEquals(2, contacts.size());
+        assertTrue(contacts.contains(person1));
+        assertTrue(contacts.contains(person2));
+    }
+
+
+    @Test
+    public void testAddNullEvent() {
+        // Adding a null event should do nothing
+        contactManager.addEvent(null);
+        List<Event> events = contactManager.getAllEvents();
+
+        assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void testAddMultipleEvents() {
+        // Adding multiple valid events
+        Event event1 = new Event("Birthday Party", "03/10/2023", "Alice's birthday celebration");
+        Event event2 = new Event("Anniversary", "05/15/2023", "Family anniversary celebration");
+
+        contactManager.addEvent(event1);
+        contactManager.addEvent(event2);
+        List<Event> events = contactManager.getAllEvents();
+
+        assertEquals(2, events.size());
+        assertTrue(events.contains(event1));
+        assertTrue(events.contains(event2));
+    }
+
+
+
+    @Test
+    public void testUpdateNonExistentEvent() {
+        // Attempting to update a non-existent event should do nothing
+        Event updatedEvent = new Event("Anniversary", "05/15/2023", "Family anniversary celebration");
+        contactManager.updateEvent("Anniversary", updatedEvent);
+
+        List<Event> events = contactManager.getAllEvents();
+
+        assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void testUpdateNullEvent() {
+        // Attempting to update a null event should do nothing
+        contactManager.updateEvent("Birthday Party", null);
+
+        List<Event> events = contactManager.getAllEvents();
+
+        assertTrue(events.isEmpty());
+    }
+
+    @Test
+    public void testUpdateEventDescription() {
+        // Creating an event
+        Event event = new Event("Birthday Party", "03/10/2023", "Alice's birthday celebration");
+        contactManager.addEvent(event);
+
+        // Updating the event's description
+        Event updatedEvent = new Event("Birthday Party", "03/10/2023", "Updated description");
+        contactManager.updateEvent("Birthday Party", updatedEvent);
+
+        Event retrievedEvent = contactManager.getEventByName("Birthday Party");
+        assertEquals("Updated description", retrievedEvent.getDescription());
+    }
+
+
+
+    @Test
+    public void testGetPersonByNameWithNullName() {
+        // Attempting to retrieve a person with a null name should return null
+        Person retrievedPerson = contactManager.getPersonByName(null);
+        assertNull(retrievedPerson);
+    }
+
+    @Test
+    public void testGetEventByName() {
+        // Adding an event to the contact manager
+        Event event1 = new Event("Birthday", "2023-01-01", "Party");
+        contactManager.addEvent(event1);
+
+        // Retrieving the event by name
+        Event retrievedEvent = contactManager.getEventByName("Birthday");
+        assertNotNull(retrievedEvent);
+        assertEquals("Birthday", retrievedEvent.getEventName());
+        assertEquals("2023-01-01", retrievedEvent.getEventDate());
+        assertEquals("Party", retrievedEvent.getDescription());
+    }
+
+    @Test
+    public void testGetEventByNameNotFound() {
+        // Retrieving an event that does not exist
+        Event retrievedEvent = contactManager.getEventByName("Anniversary");
+        assertNull(retrievedEvent);
+    }
+
+    @Test
+    public void testGetEventByNameWithNullName() {
+        // Attempting to retrieve an event with a null name should return null
+        Event retrievedEvent = contactManager.getEventByName(null);
+        assertNull(retrievedEvent);
+    }
+
+    @Test
+    public void testGetPersonByName() {
+        // Adding a person to the contact manager
+        Person person1 = new Person("Alice", "Sister", "2000-01-01", "alice@example.com", "123-456-7890");
+        contactManager.addPerson(person1);
+
+        // Retrieving the person by name
+        Person retrievedPerson = contactManager.getPersonByName("Alice");
+        assertNotNull(retrievedPerson);
+        assertEquals("Alice", retrievedPerson.getName());
+        assertEquals("Sister", retrievedPerson.getRelationship());
+        assertEquals("2000-01-01", retrievedPerson.getBirthday());
+        assertEquals("alice@example.com", retrievedPerson.getEmail());
+        assertEquals("123-456-7890", retrievedPerson.getPhoneNumber());
+    }
 }
+
+
+
+
+
+
