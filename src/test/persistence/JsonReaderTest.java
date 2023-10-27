1f9355b306023095a24ff5462e8acc151b89a6a6
daksh <dmathu01@students.cs.ubc.ca>
2023-10-27 01:52:18 -0700

    - Added the details of my project which answer the 3 basic questions given under Task 2 of Phase 0 of the project. - Added the required number and types of user stories - Phase 1 completed with necessary requirements - Task 2 of Phase 2 completed

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -54,21 +54,42 @@ class JsonReaderTest extends JsonTest {
         try {
             FamilyContactManager manager = jsonReader.read();
 
-            Event event = new Event("Test Event", "2023-12-31", "Test description");
-            manager.addEvent(event);
+            // Create a JSON array representing a list of events
+            JSONArray eventsArray = new JSONArray();
+            JSONObject event1 = new JSONObject();
+            event1.put("eventName", "Event 1");
+            event1.put("eventDate", "2023-12-31");
+            event1.put("eventDescription", "Description 1");
+            eventsArray.put(event1);
+
+            JSONObject event2 = new JSONObject();
+            event2.put("eventName", "Event 2");
+            event2.put("eventDate", "2023-11-15");
+            event2.put("eventDescription", "Description 2");
+            eventsArray.put(event2);
+
+            // Call the addEvents method to parse and add events
+            jsonReader.addEvents(manager, eventsArray);
 
             // Get the list of events from the FamilyContactManager
             List<Event> events = manager.getAllEvents();
 
-            assertEquals(1, events.size());
+            // Check that the correct number of events was added
+            assertEquals(2, events.size());
 
-            // Check the details of the added event
-            Event addedEvent = events.get(0);
-            assertEquals("Test Event", addedEvent.getEventName());
-            assertEquals("2023-12-31", addedEvent.getEventDate());
-            assertEquals("Test description", addedEvent.getEventDescription());
+            // Check the details of the added events
+            Event addedEvent1 = events.get(0);
+            assertEquals("Event 1", addedEvent1.getEventName());
+            assertEquals("2023-12-31", addedEvent1.getEventDate());
+            assertEquals("Description 1", addedEvent1.getEventDescription());
+
+            Event addedEvent2 = events.get(1);
+            assertEquals("Event 2", addedEvent2.getEventName());
+            assertEquals("2023-11-15", addedEvent2.getEventDate());
+            assertEquals("Description 2", addedEvent2.getEventDescription());
         } catch (IOException e) {
             fail("IOException should not have been thrown");
         }
     }
+
 }
