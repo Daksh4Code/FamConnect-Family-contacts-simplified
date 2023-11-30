2558629e765a2d199733e52bb16e5a71a7018ffe
daksh <dmathu01@students.cs.ubc.ca>
2023-11-30 03:53:31 -0800

    - Added the details of my project which answer the 3 basic questions given under Task 2 of Phase 0 of the project. - Added the required number and types of user stories - Phase 1 completed with necessary requirements - Task 2 of Phase 2 completed - Amended Phase 2 before starting phase 3 properly but did not remove anything from before - Phase 3 midway commit - Phase 3 completed - Made some final changes to Phase 3 - Committing and pushing my code to GitHub once before starting work on Phase 4 - Phase 4: Task 2

--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // Unused test class
 public class JsonTest {
-    // Not in use for this development phase of the project
+    // This test is not in use for this development phase of the project
     protected void checkFamilyContactManager(int expectedContacts, int expectedEvents, FamilyContactManager manager, Person person) {
         assertEquals(expectedContacts, manager.getAllContacts().size());
         assertEquals(expectedEvents, person.getEvents().size());
